ba8d48ed8e8a9c975faa9caf9dab8852c924e01e
datfoi <jengjiening@gmail.com>
2023-12-01 10:42:06 +0700

    feat: assignment-1-6/fai

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,6 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
 
-// Start coding here
+for (let i in companyName) {
+  console.log("Number " + (Number(i) + 1) + " character is " + companyName[i]);
+}
